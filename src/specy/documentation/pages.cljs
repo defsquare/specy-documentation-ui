e526630f8c3354ac60301c463b5f7b5c40ff85d3
davidpanza <david.panza@gmail.com>
2021-02-27 23:11:41 +0100

    Refactor to handle stringified building-block.kind in context

--- src/specy/documentation/pages.cljs
+++ src/specy/documentation/pages.cljs
@@ -27,7 +27,7 @@
   [:div type])
 
 (defn- type-tag [{:keys [kind] :as building-block}]
-  (condp = kind
+  (condp = (keyword kind)
     :referential
     [:span {:class "inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-purple-100 text-purple-800 capitalize"} "referential"]
 
@@ -137,6 +137,7 @@
             [:path {:class "st1", :d "M509.1,57.8c2.7,0,5.3,0.5,7.7,1.5c2.4,1,4.5,2.4,6.3,4.3c1.8,1.9,3.2,4.3,4.2,7.1c1,2.8,1.6,6.1,1.6,9.8 c0,0.8,0,1.6-0.1,2.4c-0.1,0.8-0.1,1.5-0.2,2.3h-30.4c0.3,2.2,0.8,4.1,1.6,5.7c0.8,1.6,1.7,2.9,2.8,4c1.1,1.1,2.3,1.8,3.7,2.3 c1.3,0.5,2.7,0.8,4.1,0.8c2.1,0,4.2-0.5,6.2-1.4c2-1,3.8-2.4,5.2-4.2l6.2,6.2c-4.7,5.6-10.5,8.3-17.6,8.3c-3.3,0-6.4-0.6-9.2-1.6 c-2.8-1.1-5.3-2.7-7.3-4.8c-2.1-2.1-3.7-4.6-4.8-7.6c-1.2-3-1.7-6.4-1.7-10.2c0-3.8,0.5-7.3,1.6-10.4c1.1-3.1,2.6-5.7,4.6-7.8 c2-2.1,4.3-3.8,6.9-4.9C503,58.4,505.9,57.8,509.1,57.8z M517.7,77c0-1.5-0.2-2.8-0.7-4.1c-0.4-1.3-1.1-2.5-1.8-3.4 c-0.8-1-1.8-1.8-2.9-2.4c-1.1-0.6-2.4-0.9-3.9-0.9c-1.4,0-2.6,0.3-3.8,0.8c-1.1,0.5-2.2,1.2-3,2.2c-0.9,0.9-1.6,2.1-2.1,3.4 c-0.5,1.3-0.9,2.8-1.1,4.5H517.7z"}]
             [:path {:class "st0", :d "M579,29.3v85h-30v-8.7h19.8V38.3h-19.7v-9H579z"}]]]]]
 
+        ;; cards
         [:div.flex.flex-col.w-0.flex-1.overflow-hidden
          (into
            [:div {:class "mt-8"}]
@@ -147,5 +148,6 @@
                          (map #(->card % {:on-click-fn (fn [] (swap! state assoc :selected-building-block-panel %))}) building-blocks))])
                 (group-by :ns context)))
 
+         ;; building block details panel
          (when-let [selected-building-block (:selected-building-block-panel @state)]
            (building-block-panel selected-building-block {:close-fn (fn [] (swap! state assoc :selected-building-block-panel nil))}))]]])))
